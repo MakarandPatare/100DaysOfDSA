8c760566277b4e41d70799aabaa9dd690893e522
Makarand Patare <makarand.patare@hcl.com>
2021-12-17 23:47:17 +0100

    Day 6: Arrays and Strings

---
+++ src/main/scala/Arrays/MeetingRoomsII.scala
@@ -0,0 +1,42 @@
+package Arrays
+
+/**
+ * Given an array of meeting time intervals intervals where intervals[i] = [starti, endi], return the minimum number of conference rooms required.
+
+
+
+Example 1:
+
+Input: intervals = [[0,30],[5,10],[15,20]]
+Output: 2
+Example 2:
+
+Input: intervals = [[7,10],[2,4]]
+Output: 1
+
+
+Constraints:
+
+1 <= intervals.length <= 104
+0 <= starti < endi <= 106
+ */
+
+object MeetingRoomsII {
+  def minMeetingRooms(intervals: Array[Array[Int]]): Int = {
+    import scala.collection.mutable.PriorityQueue
+
+    if (intervals.length == 0) return 0
+
+    val sIntervals = intervals.sortBy(_(0))
+    val pQueue: PriorityQueue[Int] = PriorityQueue.empty[Int](implicitly[Ordering[Int]].reverse)
+    pQueue += sIntervals(0)(1)
+
+    for (i <- 1 until sIntervals.length) {
+      if (sIntervals(i)(0) >= pQueue.head) {
+        pQueue.dequeue
+      }
+      pQueue += sIntervals(i)(1)
+    }
+    pQueue.length
+  }
+}
