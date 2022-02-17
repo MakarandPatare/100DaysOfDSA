3a406afc8846cd5243aa33983fc454baaadc7c77
Makarand Patare <makarand.patare@hcl.com>
2022-02-17 15:27:42 +0100

    Blind75 - 7: Arrays - Minor refactoring

--- src/main/scala/Blind75/Array/ProductOfArrayExceptSelf.scala
+++ src/main/scala/Blind75/Array/ProductOfArrayExceptSelf.scala
@@ -42,12 +42,12 @@ object ProductOfArrayExceptSelf {
 
   def productWithoutUsingDivision(nums: Array[Int]): Array[Int] = {
     val productUntil = new Array[Int](nums.length)
+    var productRight = 1
+    val resultArray = new Array[Int](nums.length)
     productUntil(0) = 1
     for (i <- 1 until nums.length) {
       productUntil(i) = productUntil(i - 1) * nums(i - 1)
     }
-    var productRight = 1
-    val resultArray = new Array[Int](nums.length)
     for (i <- nums.length - 1 to 0 by -1) {
       resultArray(i) = productUntil(i) * productRight
       productRight *= nums(i)
