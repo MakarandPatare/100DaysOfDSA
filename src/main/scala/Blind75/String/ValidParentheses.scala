1ec4d1aa1bde8493367661b2f0fd6a60c69c42fc
Makarand Patare <makarand.patare@hcl.com>
2022-04-14 19:14:48 +0200

    Blind75 - 24: Minor refactoring and fixing errors

--- src/main/scala/Blind75/String/ValidParentheses.scala
+++ src/main/scala/Blind75/String/ValidParentheses.scala
@@ -1,9 +1,11 @@
 package Blind75.String
 
+import scala.collection.mutable
+
 object ValidParentheses {
   def isValid(s: String): Boolean = {
     val mappings = Map(')' -> '(', '}' -> '{', ']' -> '[')
-    val stack: Stack[Character] = new Stack[Character]()
+    val stack: mutable.Stack[Character] = new mutable.Stack[Character]()
     for (i <- 0 until s.length) {
       if (mappings.contains(s(i))) {
         if (stack.isEmpty || mappings(s(i)) != stack.pop())
