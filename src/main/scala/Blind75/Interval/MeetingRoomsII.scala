0442f2925d1014903bfe76073a43b1a2f123ced4
Makarand Patare <makarand.patare@hcl.com>
2022-04-14 19:44:30 +0200

    Blind75 - 29: Bookkeeping - Add missing problem statements

--- src/main/scala/Blind75/Interval/MeetingRoomsII.scala
+++ src/main/scala/Blind75/Interval/MeetingRoomsII.scala
@@ -1,5 +1,26 @@
 package Blind75.Interval
 
+/**
+ * Given an array of meeting time intervals intervals where intervals[i] = [starti, endi], return the minimum number of conference rooms required.
+
+ 
+
+Example 1:
+
+Input: intervals = [[0,30],[5,10],[15,20]]
+Output: 2
+Example 2:
+
+Input: intervals = [[7,10],[2,4]]
+Output: 1
+ 
+
+Constraints:
+
+1 <= intervals.length <= 104
+0 <= starti < endi <= 106
+ */
+
 object MeetingRoomsII {
   def minMeetingRooms(intervals: Array[Array[Int]]): Int = {
     import scala.collection.mutable.PriorityQueue
